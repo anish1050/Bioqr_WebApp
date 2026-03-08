1a8a3cc9c3153336ce67a07fbe0ef8070c6776c9
Anish <anish@mail.com>
2026-03-08 19:01:57 +0530

    update

--- app/src/main/java/com/example/bioqr/PortraitCaptureActivity.java
+++
@@ -1,7 +0,0 @@
-package com.example.bioqr;
-
-import com.journeyapps.barcodescanner.CaptureActivity;
-
-public class PortraitCaptureActivity extends CaptureActivity {
-    // Empty class - just needed to set orientation in Manifest
-}
